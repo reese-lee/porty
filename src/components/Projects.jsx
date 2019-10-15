1a38091380d08a75f34eb75ff5c2f69af9111f67
Reese Lee <reese.lee.cy@gmail.com>
2019-10-15 14:41:28 -0700

    add initial Projects component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Projects() {
+    return (
+        <div>
+            <h1 class="header">Projects</h1>
+        </div>
+    );
+}
+
+export default Projects;
