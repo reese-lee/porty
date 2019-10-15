49ce77fd7a92c77d7ef27759b74b2efcc4621873
Reese Lee <reese.lee.cy@gmail.com>
2019-10-15 14:41:15 -0700

    add initial Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Home.css';
+import { Link } from 'react-router-dom';
+
+export class Home extends Component {
+
+  render () {
+    return (
+      <div>
+          <div>
+          <h1 class="header"><Link class="link" to="/projects">Get on top.</Link></h1>
+          </div>
+          
+      
+      </div>
+    );
+  }
+}
