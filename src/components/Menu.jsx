331c8104d3cd173f8b88349780dc2823c199f574
Reese Lee <reese.lee.cy@gmail.com>
2019-10-21 16:11:40 -0700

    change page titles and add links:

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 function Menu() {
     return (
@@ -11,15 +12,9 @@ function Menu() {
             <h5 class="bio">
                 I love building out a beautiful and functional front end, as well as solving the challenges of back end logic. 
             </h5>
-            <h5 class="titles">
-                Projects
-            </h5>
-            <h5 class="titles">
-                About
-            </h5>
-            <h5 class="titles">
-                Contact
-            </h5>
+                <span class="titles"><Link class="link" to="/projects">Projects</Link></span><span class="titles"><Link class="link" to="/menu">About</Link></span>
+                <span class="titles"><Link class="link" to="/menu">Contact</Link></span>
+                <span class="titles"><Link class="link" to="/menu">Resume</Link></span>
         </div>
         </div>
     );
