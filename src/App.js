d045343ed088d8cd0320b21bddddf99c11c2eee7
Reese Lee <reese.lee.cy@gmail.com>
2019-10-21 16:12:27 -0700

    update path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import { Home } from './components/Home';
-import  Projects  from './components/Projects';
+import  Menu  from './components/Menu';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Route exact path='/' component={Home} />
-        <Route path='/projects' component={Projects}/>
+        <Route path='/menu' component={Menu}/>
       </BrowserRouter>
       
     </div>
