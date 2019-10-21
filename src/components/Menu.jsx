1e9e05d9066a7471c1e7597be17f92137ab54ad1
Reese Lee <reese.lee.cy@gmail.com>
2019-10-21 15:10:35 -0700

    add short intro

---
+++ src/components/Menu.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Home.css';
+
+function Menu() {
+    return (
+        <div>
+        <div>
+            <h2 id="intro">
+                Hi! I'm Reese.
+            </h2>
+            <h5 class="bio">
+                I love building out a beautiful and functional front end, as well as solving the challenges of back end logic. 
+            </h5>
+            <h5 class="titles">
+                Projects
+            </h5>
+            <h5 class="titles">
+                About
+            </h5>
+            <h5 class="titles">
+                Contact
+            </h5>
+        </div>
+        </div>
+    );
+}
+
+export default Menu;
